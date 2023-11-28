581c301e0e180dbde425a6f14953bff7d5d1e53c
yasermoamd <yasermoamd@hotmail.com>
2023-11-28 01:49:13 -0800

    concat edges

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:66], str[107:112],str[0:6])
+ print(str[str.find("object"):].split()[0] + " programming  with Python")
