6871beb15c98b3d6f0fda6b76b185bfe67113faf
yasermoamd <yasermoamd@hotmail.com>
2024-03-13 05:21:39 -0700

    computes x to power of y

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    pow: function computes a to power of b and return value
+    @param: a and b
+    return: return value
+"""
+def pow(a, b):
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / pow(a, -b)
+    elif b % 2 == 0:
+        half_pow = pow(a, b //2)
+        return half_pow * half_pow
+    else:
+        return a * pow(a,b - 1)
