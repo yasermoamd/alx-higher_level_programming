b06fe30964536e49196215108cf844c8eded25d3
yasermoamd <yasermoamd@hotmail.com>
2024-03-14 05:24:18 -0700

    update: calc oprator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-add = __import__('calculator_1').add
-sub = __import__('calculator_1').sub
-mul = __import__('calculator_1').mul
-div = __import__('calculator_1').div
-
-
 if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} + {} = {}".format(a, b, sub(a, b)))
-    print("{} + {} = {}".format(a, b, mul(a, b)))
-    print("{} + {} = {}".format(a, b, div(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
