6b8d70a47cb94df889ad879d6193fda794cb7dba
yasermoamd <yasermoamd@hotmail.com>
2024-03-16 16:31:55 -0700

    fix

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,7 +11,7 @@
  */
 typedef struct listint_s
 {
-    int n;
+    int data;
     struct listint_s *next;
 } listint_t;
 
