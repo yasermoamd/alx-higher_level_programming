6d9d61f2e695d07cb563f3798fb8f37d3b2cb623
yasermoamd <yasermoamd@hotmail.com>
2024-03-16 04:28:00 +0000

    retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return None
-    for i in range(0, len(my_list)):
-        if idx == my_list[i]:
-            return my_list[idx]
-    return None
+    return my_list[idx]
