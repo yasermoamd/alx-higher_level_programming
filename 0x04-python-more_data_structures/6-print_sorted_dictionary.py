cb0970c86221642b8de36d7dfb4f8c9e2747e877
yasermoamd <yasermoamd@hotmail.com>
2024-03-20 19:17:14 +0000

    sort key

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+
+    # it will return list of the keys
+    sort_dic = sorted(a_dictionary)
+
+    for key in sort_dic:
+        print("{:}: {}".format(key, a_dictionary[key]))
