daee368135bb25461c11e3555b7294d011e20ce2
yasermoamd <yasermoamd@hotmail.com>
2024-03-20 11:21:23 +0000

    square a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+    result = [[num ** 2 for num in row] for row in matrix]
 
     for i in range(len(matrix)):
-        for j in range(len(matrix)):
+        for j in range(len(matrix[i])):
             result[i][j] = matrix[i][j] ** 2
     return result
