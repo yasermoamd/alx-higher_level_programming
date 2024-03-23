9d65a831d15895449c04c13c8b8acf56bdb6b0a1
yasermoamd <yasermoamd@hotmail.com>
2024-03-23 02:09:57 +0000

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -11,16 +11,16 @@ def roman_to_int(roman_string):
         'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000}
 
-    num = 0
+    n = 0
     r_digt = 0
     prev = 0
     count = 0
     for N in reversed(roman_string):
         r_digt = roman_dict[N]
         if r_digt >= prev:
-            num += r_digt
+            n += r_digt
         elif r_digt < prev:
-            num -= r_digt
+            n -= r_digt
         prev = r_digt
 
-    return num
+    return n
