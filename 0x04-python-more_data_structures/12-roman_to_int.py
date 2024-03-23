1758d8b452fa921e77bd9bf7ae5a0434351a7533
yasermoamd <yasermoamd@hotmail.com>
2024-03-23 02:13:33 +0000

    fix: roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 
 def roman_to_int(roman_string):
 
-    if roman_string is None or type(roman_string) != str:
+    if  not isinstance(roman_string, str) or roman_string is None:
         return 0
 
     roman_dict = {
