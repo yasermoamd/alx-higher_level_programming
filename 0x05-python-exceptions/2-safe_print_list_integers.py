78bbdf8e7f8934871a2502779f5eb56190a2f076
yasermoamd <yasermoamd@hotmail.com>
2024-04-02 16:20:13 +0100

    print list of interger with format

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     count_length = 0
     for i in range(0, x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             count_length += 1
         except (ValueError, TypeError):
             continue
