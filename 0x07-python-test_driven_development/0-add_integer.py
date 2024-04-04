ed602a2449f23c571b19ae4a89b86f9c34015467
yasermoamd <yasermoamd@hotmail.com>
2024-04-04 19:57:29 +0100

    add integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """ function add tow integer """
+    try:
+        if type(a) not in (int, float):
+            raise TypeError("a must be an integer")
+        if type(b) not in (int, float):
+            raise TypeError("a must be an integer")
+        else:
+            return a + b
+    except TypeError:
+        raise TypeError("a must be an integer")
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/0-add_integer.txt")
+
