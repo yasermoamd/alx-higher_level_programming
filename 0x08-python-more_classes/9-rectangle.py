81bbf962430717bdf1db5090c1e88dae110348ba
yasermoamd <yasermoamd@hotmail.com>
2024-04-10 11:10:45 +0100

    new instance with width==height

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -91,7 +91,7 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
-    
+
     @classmethod
     def square(cls, size=0):
         """return a new Rectangle instance with width == height == size"""
