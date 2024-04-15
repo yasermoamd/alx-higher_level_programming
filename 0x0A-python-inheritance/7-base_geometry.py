5a93298dfbb81ef312b7375bfa6cc8aaa508d00d
yasermoamd <yasermoamd@hotmail.com>
2024-04-15 16:06:11 +0100

    fix: white space

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry:
         """Public instance method that raises an Exception with the
             message area() is not implemented"""
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """Public instance method that validates value"""
         if not isinstance(value, int):
