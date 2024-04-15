04238a1a13d8c9246fb87816ec6b818533c86077
yasermoamd <yasermoamd@hotmail.com>
2024-04-15 15:49:08 +0100

    function return 1 or 0 if exactly an instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,4 +5,6 @@
 def is_same_class(obj, a_class):
     """Write a function that returns True if the object is exactly
         an instance of the specified class ; otherwise False."""
-    return type(obj) == a_class
+    if type(obj) is a_class:
+        return True
+    return False
