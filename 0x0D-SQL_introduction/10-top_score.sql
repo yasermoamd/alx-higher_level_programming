de172df3eea2cff9ff5fc10a5b3670d43a8cacea
yasermoamd <yasermoamd@hotmail.com>
2024-06-15 11:53:05 +0000

    task

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- List table items ordered by score
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC 
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Count items with id 89
+SELECT COUNT(*) FROM first_table WHERE(id=89)
