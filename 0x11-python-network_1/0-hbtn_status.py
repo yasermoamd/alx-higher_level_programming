b8989dc1108655a467c64fc106350b151aa9eb30
yasermoamd <yasermoamd@hotmail.com>
2024-07-05 23:41:19 -0700

    fetch status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
 from urllib.request import Request, urlopen
 
 
 if __name__ == "__main__":
-    """fetches https://intranet.hbtn.io/status"""
-    req_status = Request("https://intranet.hbtn.io/status")
+    req = Request("https://alx-intranet.hbtn.io/status")
     with urlopen(req) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
