4559689c16fd2e6d78d4219537f3295b419cf7e7
yasermoamd <yasermoamd@hotmail.com>
2024-07-05 23:37:50 -0700

    fetch status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
-import urllib.request
+from urllib.request import Request, urlopen
 
 
 if __name__ == "__main__":
     """fetches https://intranet.hbtn.io/status"""
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    req_status = Request("https://intranet.hbtn.io/status")
+    with urlopen(req) as response:
         body = response.read()
-        body_str = body.decode('utf-8')
-
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body_str))
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
